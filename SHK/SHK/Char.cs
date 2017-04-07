827c2eb8c88f99683852d31d65cb6a36643147de
Carlos Costa <washinima99@gmail.com>
2017-04-07 22:36:14 +0100

    Initial Set up

---
+++ SHK/SHK/Char.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHK
+{
+    class Char
+    {
+        //animation bools Not sure yet
+        bool attack;
+        bool jumping;
+        bool idle;
+
+
+
+    }
+}
