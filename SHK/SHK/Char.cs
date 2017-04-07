91b234116c0b52284006bf6ab6f3913412484fe6
Carlos Costa <washinima99@gmail.com>
2017-04-07 23:59:22 +0100

    fix2 maybe

--- SHK/SHK/Char.cs
+++ SHK/SHK/Char.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace SHK
 {
     class Char
     {
         //animation bools Not sure yet
-        bool attack;
-        bool jumping;
-        bool idle;
+        bool attack = false;
+        bool jumping = false;
+        bool idle = true;
+
+        public Texture2D main;
+
+        public Char(string imageName ,Vector2 position)
+        {
+            //ouch
+        }
+
 
 
 
