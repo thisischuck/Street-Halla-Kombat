23f68e2758eba4837883e917263d2c1f4ed1d181
Carlos Costa <washinima99@gmail.com>
2017-05-26 10:54:30 +0100

    KILL ME NOW

--- SHK/SHK/Projectil.cs
+++ SHK/SHK/Projectil.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SHK
 {
-    class Projectil
+    class Projectil : SpritePrimitive
     {
+        public Projectil(Vector2 position, Vector2 size) : base("a", position, size, 0, 0, 0, SpriteEffects.None)
+        {
+            
+        }
     }
 }
